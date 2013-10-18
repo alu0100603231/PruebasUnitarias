1be55a873811b2d79663a0512653d7657b0ce5ff
Sergio Oramas <alu0100603231@ull.edu.es>
2013-10-18 17:59:59 +0100

    test

--- tc_racional.rb
+++ tc_racional.rb
@@ -1,8 +1,47 @@
-# Implementar en este fichero las Pruebas Unitarias asociadas a la clase Fraccion
-
 require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
 
+        def setup
+                @a = Fraccion.new(3,2)
+                @b = Fraccion.new(2,2)
+        end
+
+       
+        def test_suma
+                #Comprobamos la suma con los valores estandar
+                result = @a.suma(@b)
+                assert result.numerador == 5 and result.denominador == 2
+              
+              
+        end
+        def test_resta
+                #Comprobamos la resta con los valores estandar
+                result = @a.resta(@b)
+                assert result.numerador == 1 and result.denominador == 2
+               
+        end
+        def test_producto
+                #Comprobamos el producto con los valores estandar
+                result = @a.producto(@b)
+                assert result.numerador == 3 and result.denominador == 2
+               
+
+        end
+        def test_division
+                #Comprobamos la division con los valores estandar
+                result = @a.division(@b)
+                assert result.numerador == 3 and result.denominador == 2
+                
+        end
+
+
 end
+
+
+
+
+
+
+
