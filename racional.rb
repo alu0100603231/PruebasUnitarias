a108952a7f1cfe573fcbc1c6cec40af6dc8dbaa4
Sergio Oramas <alu0100603231@ull.edu.es>
2013-10-18 17:45:00 +0100

    programafinal

--- racional.rb
+++ racional.rb
@@ -1,6 +1,42 @@
-# Implementar en este fichero la clase para crear objetos racionales
-
 require "./gcd.rb"
 
 class Fraccion
+
+  attr_accessor :numerador ,  :denominador
+
+  def initialize(numerador, denominador)
+    #Controlando que los numeros sean enteros, y que el denominador no sea 0
+    mcd = gcd(numerador, denominador) # maximo comun divisor
+    @numerador = numerador / mcd # fraccionsimplificada
+    @denominador = denominador / mcd
+
+  end
+
+
+  def to_s
+    "#{@numerador}/#{@denominador}"
+  end
+
+  def suma(otra)
+        result = Fraccion.new(@numerador*otra.denominador + @denominador*otra.numerador, @denominador*otra.denominador)
+  end
+
+  def resta(otra)
+       result = Fraccion.new(@numerador*otra.denominador - @denominador*otra.numerador, @denominador*otra.denominador)
+  end
+
+  def producto(otra)
+        result = Fraccion.new(@numerador*otra.numerador, @denominador*otra.denominador)
+  end
+
+  def division(otra)
+        result = Fraccion.new(@numerador*otra.denominador, @denominador*otra.numerador)
+ end
+
 end
+
+
+
+
+
+
